c2a724cd9e7a0b30d6abf36ce602a2e18340d887
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-07-24 00:15:21 +0530

    learn about :any

---
+++ TypeScript/Assignment/any.ts
@@ -0,0 +1,9 @@
+// any is a type that disables type checking and effectively allows all types to be used.
+
+// let isBool:any = true;
+// isBool = "true";
+// console.log(isBool)
+
+let isBool = true;
+isBool = false;
+console.log(isBool)
